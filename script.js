9de12bdce8d7aa83038842d57a04319b4ca2d88e
Sandra Borboa <sborboaluna@gmail.com>
2025-11-23 23:15:36 -0700

    Feat: Allows user to input City name

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 console.log("Hola API clima");
 
+// Usando GeoCoding API para obtener las coordenadas de la Ciudad
+async function getCoordinates(cityName) {
+    const geoUrl = `https://geocoding-api.open-meteo.com/v1/search?name=${cityName}&count=10&language=en&format=json`;
+    const geoResponse = await fetch(geoUrl);
+    const geodata = await geoResponse.json();
+    
+    if (!geodata.results || geodata.results.length === 0) {
+        throw new Error("City not found, try again.");
+    }
+
+    const firstCity = geodata.results[0]; // Elige el primer resultado que coincida con el input
+    return firstCity;
+
+}
+
 async function fetchWeatherData(latitude, longitude) { // async la convierte en asíncrona
     const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`;
     const response = await fetch(url); //await le indica a la función que se espere a que termine de ejecutarse para continuar con el proceso.
@@ -9,18 +24,28 @@ async function fetchWeatherData(latitude, longitude) { // async la convierte en 
     console.log(data.elevation);
     console.log(data.current_weather);
     console.log(data.current_weather.temperature);
-    return data.current_weather;
+    return data.current_weather; // Devuelve objeto de la API, no se asignan keys.
 }
 
 // fetchWeatherData(25.666815, -100.28233);
-async function handleFetchClick(){ // le da funcionalidad al botón
+
+async function handleFetchClick(){ // Le da funcionalidad al botón
     console.log("Fetch button clicked");
-    const latitude = document.getElementById("latitude-input").value; // se accede a la api dom mediante document, usando el metodo getElementByID, se añade value para leer el valor.
-    const longitude = document.getElementById("longitude-input").value;
+
+    const cityName = document.getElementById("city-input").value; // Se accede a la API dom mediante document, usando el metodo getElementByID, se añade value para leer el valor.
+
+    const firstCity = await getCoordinates(cityName); // Obtiene las coordenadas
+    const latitude = firstCity.latitude; // Asigna las coordenadas a las variables
+    const longitude = firstCity.longitude;
+    
+    /*
+    const latitude = document.getElementById("latitude-input").value; // se accede a la api dom mediante document, usando el metodo getElementByID, se añade value para leer el valor. */
+
     const currentTemparature = document.getElementById("temp-display");
     const currentWind = document.getElementById("wind-display");
 
     const currentWeather = await fetchWeatherData(latitude, longitude);
-    currentTemparature.textContent = currentWeather.temperature; //referencia a lo que se encuentra entre los tags del html
+
+    currentTemparature.textContent = currentWeather.temperature; // Referencia a lo que se encuentra entre los tags del html
     currentWind.textContent = currentWeather.windspeed;
 }
