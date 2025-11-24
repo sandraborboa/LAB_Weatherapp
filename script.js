5e936ae04af7b79cbe8b17fcf2dcd7922ee10fe7
Sandra Borboa <sborboaluna@gmail.com>
2025-11-24 00:20:21 -0700

    Feat: Adds UV Index

--- script.js
+++ script.js
@@ -16,15 +16,26 @@ async function getCoordinates(cityName) {
 }
 
 async function fetchWeatherData(latitude, longitude) { // async la convierte en asíncrona
-    const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`;
+    // const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`;
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,rain,wind_speed_10m&hourly=uv_index&timezone=auto&forecast_hours=1`;
     const response = await fetch(url); //await le indica a la función que se espere a que termine de ejecutarse para continuar con el proceso.
     console.log(response); 
     const data = await response.json();
     console.log(data);
-    console.log(data.elevation);
-    console.log(data.current_weather);
-    console.log(data.current_weather.temperature);
-    return data.current_weather; // Devuelve objeto de la API, no se asignan keys.
+
+    const temperature = data.current.temperature_2m;
+    const windspeed = data.current.wind_speed_10m;
+    const uvindex = data.hourly.uv_index;
+
+    // console.log(data.elevation);
+    // console.log(data.current_weather);
+    // console.log(data.current_weather.temperature);
+    // return data.current_weather; // Devuelve objeto de la API, no se asignan keys.
+    return {
+        temperature,
+        windspeed,
+        uvindex
+    }
 }
 
 // fetchWeatherData(25.666815, -100.28233);
@@ -34,6 +45,7 @@ async function handleFetchClick(){ // Le da funcionalidad al botón
 
     const cityName = document.getElementById("city-input").value; // Se accede a la API dom mediante document, usando el metodo getElementByID, se añade value para leer el valor.
 
+    // Invoca al método getCoordinates
     const firstCity = await getCoordinates(cityName); // Obtiene las coordenadas
     const latitude = firstCity.latitude; // Asigna las coordenadas a las variables
     const longitude = firstCity.longitude;
@@ -41,11 +53,14 @@ async function handleFetchClick(){ // Le da funcionalidad al botón
     /*
     const latitude = document.getElementById("latitude-input").value; // se accede a la api dom mediante document, usando el metodo getElementByID, se añade value para leer el valor. */
 
-    const currentTemparature = document.getElementById("temp-display");
+    const currentTemperature = document.getElementById("temp-display");
     const currentWind = document.getElementById("wind-display");
+    const currentUV = document.getElementById("uv-display");
 
+    // Invoca el método fetchWeatherData usando como argumentos los returns de getCoordinates()
     const currentWeather = await fetchWeatherData(latitude, longitude);
 
-    currentTemparature.textContent = currentWeather.temperature; // Referencia a lo que se encuentra entre los tags del html
+    currentTemperature.textContent = currentWeather.temperature; // Referencia a lo que se encuentra entre los tags del html
     currentWind.textContent = currentWeather.windspeed;
+    currentUV.textContent = currentWeather.uvindex;
 }
